a12beb0aa6fe707bf39273b71aeb1f98302d9854
rodrigodesousa <rodrigudesousa@gmail.com>
2021-07-23 11:09:28 -0400

    first commit

---
+++ AutenticacaoAspNetMvc/Controllers/AutenticacaoController.cs
@@ -0,0 +1,45 @@
+﻿using AutenticacaoAspNetMvc.Models;
+using AutenticacaoAspNetMvc.Utils;
+using AutenticacaoAspNetMvc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutenticacaoAspNetMvc.Controllers
+{
+    public class AutenticacaoController : Controller
+    {
+        private UsuariosContext db = new UsuariosContext();
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Cadastrar(CadastroUsuarioViewModel viewmodel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewmodel);
+            }
+            if (db.Usuarios.Count(u => u.Login == viewmodel.Login) > 0)
+            {
+                ModelState.AddModelError("Login", "Esse login já está em uso");
+                return View(viewmodel);
+            }
+
+            Usuario novoUsuario = new Usuario
+            {
+                Nome = viewmodel.Nome,
+                Login = viewmodel.Login,
+                Senha = Hash.GerarHash(viewmodel.Senha)
+            };
+
+            db.Usuarios.Add(novoUsuario);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
